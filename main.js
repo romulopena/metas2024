493cabc144c63a2e73ae804cd804c02baf07c8ea
PROF ROMULO <rjlpena@gmail.com>
2024-03-08 07:36:31 -0300

    ok

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ console.log(botoes);
 //laço de repetição FOR
 for(let i = 0; i <botoes.length; i++ );
 console.log(i);
+
+//ativação por clique
+botoes[i].onclick = function(){
+    
+    botoes[i].classList.add('ativo');
+}
