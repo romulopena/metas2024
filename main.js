193f32e74fc05637362848d9fd5caae116f9b09e
PROF ROMULO <rjlpena@gmail.com>
2024-03-08 07:40:19 -0300

    ok

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 const botoes = document.querySelectorAll(".botao");
-console.log(botoes);
+console.log(botoes)
 
 //laço de repetição FOR
-for(let i = 0; i <botoes.length; i++ );
-console.log(i);
-
+for (let i = 0; i < botoes.length; i++);
+console.log(i)
 //ativação por clique
-botoes[i].onclick = function(){
-    
-    botoes[i].classList.add('ativo');
+botoes[i].onclick = function () {
+
+
+
+    for (let j = 0; j < botoes.length; j++) {
+        botoes[j].classList.remove("ativo");
+
+        botoes[i].classList.add('ativo');
+
+    }
+
 }
