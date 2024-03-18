5cc0884adc84725874bf0e7d61f2bdbc913d30ef
PROF ROMULO <rjlpena@gmail.com>
2024-03-18 10:38:26 -0300

    ok

--- main.js
+++ main.js
@@ -16,5 +16,6 @@ for(let i=0; i <botoes.length; i++){
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-12-31T00:00:00");
-contadores[0].textContent = tempoObjetivo1;
+let tempoAtual = new Date();
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
 
