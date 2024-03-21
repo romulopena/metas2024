47335465d6107558d62e321707f08607438cc0bb
PROF ROMULO <rjlpena@gmail.com>
2024-03-21 08:44:36 -0300

    ok

--- main.js
+++ main.js
@@ -13,7 +13,8 @@ for(let i=0; i <botoes.length; i++){
     }
 }
 const tempoObjetivo1 = new Date("2024-12-21T00:00:00");
-contadores[0].textContent = tempoObjetivo1;
+let tempoAtual = new Date();
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
 
 
 
