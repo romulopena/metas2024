1ef7fa0fa0b057716334fccb4c11521974eaf17e
PROF ROMULO <rjlpena@gmail.com>
2024-03-21 08:34:30 -0300

    ok

--- main.js
+++ main.js
@@ -12,8 +12,8 @@ for(let i=0; i <botoes.length; i++){
         textos[i].classList.add("ativo");
     }
 }
+const tempoObjetivo1 = new Date("2024-12-21T00:00:00");
 contadores[0].textContent = tempoObjetivo1;
-const tempoObjetivo1 = new Date("2024-12-21");
 
 
 
